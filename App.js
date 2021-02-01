f91c8972a1fd5e0728a1527c95ecab901453306f
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-02-01 10:26:28 +0530

    Chat App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = props => {
+  return(
+    <View>
+      <Text>
+        This is the Main Screen
+      </Text>
+    </View>
+  )
+}
+
+export default App;
