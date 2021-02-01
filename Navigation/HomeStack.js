f97f79c85e430ad877090065f51bdfd708f5abb2
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-02-01 13:45:38 +0530

    Fetching the Chat Rooms From Firestore Done

--- Navigation/HomeStack.js
+++ Navigation/HomeStack.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import AddRoomScreen from '../screens/AddRoomScreen';
+// import { IconButton } from 'react-native-paper';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const Stack = createStackNavigator();
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+function ChatApp() {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee',
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22,
+        },
+      }}
+    >
+      <ChatAppStack.Screen
+        name='Home'
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <Entypo
+            style={{marginRight: 5,}}
+              name='circle-with-plus'
+              size={28}
+              color='#ffffff'
+              onPress={() => navigation.navigate('AddRoom')}
+            />
+          ),
+        })}
+      />
+    </ChatAppStack.Navigator>
+  );
+}
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+    <ModalStack.Navigator mode='modal' headerMode='none'>
+      <ModalStack.Screen name='ChatApp' component={ChatApp} />
+      <ModalStack.Screen name='AddRoom' component={AddRoomScreen} />
+    </ModalStack.Navigator>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,65 @@
-import React, {useContext} from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Title } from 'react-native-paper';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Title, List, Divider } from 'react-native-paper';
 import FormButton from '../components/FormButton';
-import {AuthContext} from '../Navigation/AuthProvider';
+import { AuthContext } from '../Navigation/AuthProvider';
+import firestore from '@react-native-firebase/firestore';
+import Loading from '../components/Loading';
 
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
 
-  const {user, logout} = useContext(AuthContext);
+  const [threads, setThreads] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { user, logout } = useContext(AuthContext);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('THREADS')
+      .onSnapshot((querySnapshot) => {
+        const threads = querySnapshot.docs.map((documentSnapshot) => {
+          return {
+            _id: documentSnapshot.id,
+            // give defaults
+            name: '',
+            ...documentSnapshot.data(),
+          };
+        });
+
+        setThreads(threads);
+
+        if (loading) {
+          setLoading(false);
+        }
+      });
+
+    /**
+     * unsubscribe listener
+     */
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Title style={{color: 'black'}}>Home Screen</Title>
-      <Title style={{color: 'black'}}>All chat rooms will be listed here</Title>
-      <Title style={{color: 'black'}}>{user.uid}</Title>
-      <FormButton
-        modeValue='contained'
-        title='Logout'
-        onPress={() => logout()}
+      <FlatList
+        data={threads}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <Divider style={{backgroundColor: 'black'}} />}
+        renderItem={({ item }) => (
+          <List.Item
+            title={item.name}
+            description='Item description'
+            titleNumberOfLines={1}
+            titleStyle={styles.listTitle}
+            descriptionStyle={styles.listDescription}
+            descriptionNumberOfLines={1}
+          />
+        )}
       />
     </View>
   );
@@ -26,7 +69,13 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f5f5f5',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  listTitle: {
+    fontSize: 22,
+    color: 'black'
+  },
+  listDescription: {
+    fontSize: 16,
+    color: 'black'
+  },
 });
